dcd313ae59f63a3dd3ae835ae42d5087258fc180
Takuto Miyahara <tmtydoraemon2@gmail.com>
2024-07-16 17:21:24 +0900

    ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,37 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ゼミ活動</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-</head>
-<body>
-    <header class="bg-light p-3">
-        <div class="container d-flex justify-content-between align-items-center">
-            <a href="{{ route('index') }}">
-                <img src="{{ asset('images/logo.png') }}" alt="トップページへ戻る" id="home-icon" class="img-fluid" style="width: 50px;">
-            </a>
-            <nav>
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="btn btn-primary btn-lg m-2" href="{{ route('profile') }}">
-                            <i class="fas fa-user-graduate"></i> ゼミ生のプロフィール
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-primary btn-lg m-2" href="{{ route('activities') }}">
-                            <i class="fas fa-history"></i> 過去の活動
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+@extends('layouts.app')
 
-    <main class="container my-5">
+@section('title', 'ゼミ紹介ページ')
+
+@section('content')
+        <div class="text-center">
+            <h1>田口ゼミ紹介ページです！</h1>
+            <br>
+            <p>ゼミに関する情報や活動内容を紹介します。</p>
+        </div>
         <div id="zemiCarousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#zemiCarousel" data-slide-to="0" class="active"></li>
